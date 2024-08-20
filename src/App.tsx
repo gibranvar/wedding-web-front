7792cea273a488c332b1f91db34696f6cbbb20c6
Gibran Var <104398839+gibranvar@users.noreply.github.com>
2024-08-20 15:07:45 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
-
+import GuestTable from './views/Guests/GuestTable'
 import Home from './views/Home/Home';
 import './App.css';
 import LoadingView from './views/Loading/Loading';
-import GuestTable from './views/Guests/GuestTable'
 
 const App: React.FC = () => {
   return (
@@ -13,7 +12,7 @@ const App: React.FC = () => {
         <Routes>
           <Route path='/' element={<LoadingView/>}/>
           <Route path="/home" element={<Home />} />
-           <Route path="/guests" element={<GuestTable />} />
+          <Route path="/guests" element={<GuestTable />} />
         </Routes>
       </div>
     </Router>
